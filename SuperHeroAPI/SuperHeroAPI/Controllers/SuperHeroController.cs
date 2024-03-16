b400fa4f419657165625049cca87ce40d7458dab
Orlin221 <61077638+319190@users.noreply.github.com>
2024-03-16 13:52:05 +0200

    .NET 8 Web API
    Entity Framework, full CRUD operations , using  MySql , HTTP requests . Everything works fine,  using fat controllers for this small project.

---
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+using SuperHeroAPI.Entities;
+using System.Reflection.Metadata.Ecma335;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+           this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = await _context.SuperHeroes.ToListAsync();
+
+            return Ok(heroes);
+           
+        }
+        [HttpGet("{id}")] 
+        public async Task<ActionResult<SuperHero>> GetHero(int id)
+        {
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if (hero is null)
+                return NotFound("Hero not found.");
+
+            return Ok(hero);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+             _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok( await _context.SuperHeroes.ToListAsync());
+
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero hero)
+        {
+            var heroi = await _context.SuperHeroes.FindAsync(hero.Id);
+            if (heroi is null)
+                return NotFound("Hero not found.");
+            heroi.Name = hero.Name;
+            heroi.FirstName = hero.FirstName;
+            heroi.LastName = hero.LastName;
+            heroi.Place = hero.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
+        {
+            var heroi = await _context.SuperHeroes.FindAsync(id);
+            if (heroi is null)
+                return NotFound("Hero not found.");
+
+            _context.SuperHeroes.Remove(heroi);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+
+        }
+
+    }
+}
---
+++ SuperHeroAPI/SuperHeroAPI/Entities/SuperHero.cs
@@ -0,0 +1,15 @@
+﻿namespace SuperHeroAPI.Entities
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Place { get; set; } = string.Empty;
+    }
+}
